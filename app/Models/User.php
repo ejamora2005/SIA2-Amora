91d40e8c96cabac0c8ea017307519cb8becd6c11
Emil Jon Amora <ejamora19@gmail.com>
2026-02-10 12:03:03 +0800

    Customize users table migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'is_active',
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -40,6 +43,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'is_active' => 'boolean',
     ];
 }
